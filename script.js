4f0e95492760f4b614f810415fa14a8be0bd118a
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-06-01 15:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const h1 = document.getElementsByTagName('h1')[0];
+function updateH1Text(x, y) {
+	h1.innerText = `Width: ${x} and Height: ${y}`;
+}
+updateH1Text(window.innerWidth, window.innerHeight);
+
+window.onresize = () => {
+	updateH1Text(window.innerWidth, window.innerHeight);
+}
